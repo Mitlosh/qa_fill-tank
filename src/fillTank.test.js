97988acb1e76e52cee457f9b5b8253521ea03f55
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2026-02-23 20:05:41 +0200

    solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-describe('fillTank', () => {
-  const { fillTank } = require('./fillTank');
+describe("fillTank", () => {
+  const { fillTank } = require("./fillTank");
 
   let customer;
 
@@ -15,47 +15,47 @@ describe('fillTank', () => {
     };
   });
 
-  test('should fill full tank if amount is not provided', () => {
-    fillTank(customer, 10);
-    expect(customer.vehicle.fuelRemains).toBe(50);
-    expect(customer.money).toBe(600);
+  test("should return nothing (undefined)", () => {
+    const result = fillTank(customer, 10, 5);
+    expect(result).toBeUndefined();
   });
 
-  test('should fill only what fits in the tank', () => {
+  test("should fill only what fits in the tank", () => {
     fillTank(customer, 10, 100);
     expect(customer.vehicle.fuelRemains).toBe(50);
-    expect(customer.money).toBe(600);
+    expect(customer.money).toBeCloseTo(600, 2);
   });
 
-  test('should fill only what customer can afford', () => {
-    customer.money = 100;
-    fillTank(customer, 50);
-    expect(customer.vehicle.fuelRemains).toBe(12);
-    expect(customer.money).toBe(0);
-  });
-
-  test('should not fill if amount is less than 2 liters', () => {
-    fillTank(customer, 10, 1.9);
+  test("should not fill if affordable amount rounded is < 2", () => {
+    customer.money = 19.9;
+    fillTank(customer, 10);
     expect(customer.vehicle.fuelRemains).toBe(10);
-    expect(customer.money).toBe(1000);
+    expect(customer.money).toBeCloseTo(19.9, 2);
   });
 
-  test('should discard to the tenth part for fuel amount', () => {
+  test("should use minimum of amount, space, and money", () => {
+    customer.money = 60;
+    fillTank(customer, 20, 10);
+    expect(customer.vehicle.fuelRemains).toBe(13);
+    expect(customer.money).toBeCloseTo(0, 2);
+  });
+
+  test("should discard to the tenth part for fuel amount", () => {
     customer.money = 100;
     fillTank(customer, 30);
     expect(customer.vehicle.fuelRemains).toBe(13.3);
-    expect(customer.money).toBe(1);
+    expect(customer.money).toBeCloseTo(1, 2);
   });
 
-  test('should round price to the nearest hundredth', () => {
+  test("should round price to the nearest hundredth", () => {
     fillTank(customer, 10.123, 2.5);
-    expect(customer.money).toBe(974.69);
+    expect(customer.money).toBeCloseTo(974.69, 2);
   });
 
-  test('should handle zero money case', () => {
+  test("should handle zero money case", () => {
     customer.money = 0;
     fillTank(customer, 10, 10);
     expect(customer.vehicle.fuelRemains).toBe(10);
-    expect(customer.money).toBe(0);
+    expect(customer.money).toBeCloseTo(0, 2);
   });
 });
