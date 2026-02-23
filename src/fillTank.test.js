2a014c73a6d376b5b4eadbdc2620feb961021587
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2026-02-23 19:57:23 +0200

    solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-describe("fillTank", () => {
-  const { fillTank } = require("./fillTank");
+describe('fillTank', () => {
+  const { fillTank } = require('./fillTank');
 
   let customer;
 
@@ -15,44 +15,44 @@ describe("fillTank", () => {
     };
   });
 
-  test("should fill full tank if amount is not provided", () => {
+  test('should fill full tank if amount is not provided', () => {
     fillTank(customer, 10);
     expect(customer.vehicle.fuelRemains).toBe(50);
     expect(customer.money).toBe(600);
   });
 
-  test("should fill only what fits in the tank", () => {
+  test('should fill only what fits in the tank', () => {
     fillTank(customer, 10, 100);
     expect(customer.vehicle.fuelRemains).toBe(50);
     expect(customer.money).toBe(600);
   });
 
-  test("should fill only what customer can afford", () => {
+  test('should fill only what customer can afford', () => {
     customer.money = 100;
     fillTank(customer, 50);
     expect(customer.vehicle.fuelRemains).toBe(12);
     expect(customer.money).toBe(0);
   });
 
-  test("should not fill if amount is less than 2 liters", () => {
+  test('should not fill if amount is less than 2 liters', () => {
     fillTank(customer, 10, 1.9);
     expect(customer.vehicle.fuelRemains).toBe(10);
     expect(customer.money).toBe(1000);
   });
 
-  test("should discard to the tenth part for fuel amount", () => {
+  test('should discard to the tenth part for fuel amount', () => {
     customer.money = 100;
     fillTank(customer, 30);
     expect(customer.vehicle.fuelRemains).toBe(13.3);
     expect(customer.money).toBe(1);
   });
 
-  test("should round price to the nearest hundredth", () => {
+  test('should round price to the nearest hundredth', () => {
     fillTank(customer, 10.123, 2.5);
     expect(customer.money).toBe(974.69);
   });
 
-  test("should handle zero money case", () => {
+  test('should handle zero money case', () => {
     customer.money = 0;
     fillTank(customer, 10, 10);
     expect(customer.vehicle.fuelRemains).toBe(10);
